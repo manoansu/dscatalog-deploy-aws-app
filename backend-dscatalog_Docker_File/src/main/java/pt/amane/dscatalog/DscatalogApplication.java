46827db1aafe31b46987ced8e16425e76c32059d
Ansumane Mané <manoansu@gmail.com>
2021-10-31 11:55:56 +0000

    upload endpoint saved file in aws

--- backend-dscatalog_Docker_File/src/main/java/pt/amane/dscatalog/DscatalogApplication.java
+++ backend-dscatalog_Docker_File/src/main/java/pt/amane/dscatalog/DscatalogApplication.java
@@ -1,27 +1,23 @@
 package pt.amane.dscatalog;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import pt.amane.dscatalog.services.S3Service;
-
 @SpringBootApplication
-public class DscatalogApplication implements CommandLineRunner{
+public class DscatalogApplication { //implements CommandLineRunner{
 
-	@Autowired
-	private S3Service s3Service;
+//	@Autowired
+//	private S3Service s3Service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(DscatalogApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		
-		//chama a imagem para subir na nuven de aws...
-		s3Service.uploadFile("C:\\Developement\\img\\aws_print.PNG");
-	}
+//	@Override
+//	public void run(String... args) throws Exception {
+//		
+//		//chama a imagem para subir na nuven de aws...
+//		s3Service.uploadFile("C:\\Developement\\img\\aws_print.PNG");
+//	}
 
 }
